d33ebee71119d87acbf8d68c1ca82684a2699326
aman2e <97337137+Aman2e@users.noreply.github.com>
2026-03-25 13:22:58 +0530

    Change initial directory for file dialog

--- src.py
+++ src.py
@@ -20,7 +20,7 @@ def Down(name):
              })
     conn.commit()
     conn.close()
-    root.filename = filedialog.askopenfilename(initialdir="/home/akshay",title = "select a file",filetypes=[("mp4","*.mp4")])
+    root.filename = filedialog.askopenfilename(initialdir="/home/aman",title = "select a file",filetypes=[("mp4","*.mp4")])
     res = messagebox.askquestion("Confirmation","Do you want to play it??")
     if res =="yes":
         video(root.filename)
